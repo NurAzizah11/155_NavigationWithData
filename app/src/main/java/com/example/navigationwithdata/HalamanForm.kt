783a03df65b62e876cc7cc2c61747c570cbc6553
Nur Azizah <nurrrazizahhh11@gmail.com>
2023-11-14 19:15:13 +0700

    Menambahkan Column di HalamanForm

--- app/src/main/java/com/example/navigationwithdata/HalamanForm.kt
+++ app/src/main/java/com/example/navigationwithdata/HalamanForm.kt
@@ -1,11 +1,18 @@
 package com.example.navigationwithdata
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -21,5 +28,16 @@ fun HalamanForm(
     var telponTxt by remember {
         mutableStateOf("")
     }
+
     var lisDataTxt: MutableList<String> = mutableListOf(namaTxt, alamatTxt, telponTxt)
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ) {
+
+    }
 }
