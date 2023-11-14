e4c86b4c7de24fe012c10a118eb9cf50c76a6a83
Nur Azizah <nurrrazizahhh11@gmail.com>
2023-11-14 19:12:45 +0700

    Menambahkan fun HalamanForm

--- app/src/main/java/com/example/navigationwithdata/HalamanForm.kt
+++ app/src/main/java/com/example/navigationwithdata/HalamanForm.kt
@@ -1,2 +1,25 @@
 package com.example.navigationwithdata
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HalamanForm(
+    onSubmitButtonClick: (MutableList<String>) -> Unit
+) {
+    var namaTxt by remember {
+        mutableStateOf("")
+    }
+    var alamatTxt by remember {
+        mutableStateOf("")
+    }
+    var telponTxt by remember {
+        mutableStateOf("")
+    }
+    var lisDataTxt: MutableList<String> = mutableListOf(namaTxt, alamatTxt, telponTxt)
+}
