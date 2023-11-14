71cf7e783163d55417890c02c0beea22587cea3c
Nur Azizah <nurrrazizahhh11@gmail.com>
2023-11-14 19:29:43 +0700

    Menambahkan val nama, val alamat, val noTelp di OrderUIState

--- app/src/main/java/com/example/navigationwithdata/HalamanDua.kt
+++ app/src/main/java/com/example/navigationwithdata/HalamanDua.kt
@@ -17,6 +17,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import com.example.navigationwithdata.data.OrderUIState
 import com.example.navigationwithdata.ui.theme.komponen.FormatLabelHarga
 
